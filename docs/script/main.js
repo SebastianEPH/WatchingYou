c5958bbab86f0bdae5164cb5b100e396c6f139dc
SebastianEPH <sebastianeph99@gmail.com>
2021-01-24 18:34:03 -0500

    descripción de métodos y clases

--- docs/script/main.js
+++ docs/script/main.js
@@ -3,12 +3,16 @@ let $ficha_tecnica= document.getElementById('ficha_tecnica')
 let $manual_user= document.getElementById('manual_user')
 let $manual_docente = document.getElementById('manual_docente')
 let $main = document.getElementById('main')
+let $code_documentation = document.getElementById('code_documentation')
+let $about = document.getElementById('about')
 
 function all(){
     $manual_docente.style.display = 'none'
     $manual_user.style.display = 'none'
     $ficha_tecnica.style.display = 'none'
     $main.style.display = 'none'
+    $code_documentation.style.display = 'none'
+    $about.style.display = 'none'
 }
 function main(){
     all()
@@ -27,7 +31,14 @@ function manualDocente(){
     all()
     $manual_docente.style.display = 'block'
 }
+function code_documentation(){
+    all()
+    $code_documentation.style.display = 'block'
+}
+function about(){
+    all()
+    $about.style.display = 'block'
+}
 
-//main()
-manualDocente()
+code_documentation()
 
